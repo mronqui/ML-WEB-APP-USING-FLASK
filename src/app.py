7ff27450c0e5d0b0bbeb902c6c1e9db815ff06bb
Mauricio Ronqui <mronqui@gmail.com>
2022-08-03 23:53:26 +0000

    Con modelo de cristian

--- src/app.py
+++ src/app.py
@@ -1,4 +1,28 @@
-from utils import db_connect
-engine = db_connect()
+import numpy as np
+import flask
+import pickle
+from flask import Flask, render_template, request
 
-# your code here
+app = Flask(__name__, template_folder='')
+
+model = pickle.load(open('../models/titanic_model.pickle', 'rb'))
+
+@app.route('/') #http://www.google.com/
+def home():
+    return render_template('index.html')
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Would you survive? {} (1=survived, 0=deceased)'.format(output))
+
+if __name__=="__main__":
+    app.run(port=5000, debug=True)
+
